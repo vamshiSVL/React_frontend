345a7e34a1ac0a2d6874238fe1161258b103e4ae
Vamshi <vamshi@gowni.com>
2025-02-12 21:23:39 +0530

    change path in url

--- dashboard/src/vendordashboard/components/forms/data/apiPath.js
+++ dashboard/src/vendordashboard/components/forms/data/apiPath.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:5000";
+// export const API_URL = "http://localhost:5000";
 
-// const API_URL = "https://backend-nodejs-1-1ed8.onrender.com/vendor/list";
+const API_URL = "https://backend-nodejs-1-1ed8.onrender.com/vendor/list";
